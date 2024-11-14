36d6c838527c00e95c82a7af88a7c6dd0040c7e2
Brendan Chen <me@bchen.dev>
2024-11-13 20:51:21 -0800

    add test container sample class

---
+++ test/test_container_sample.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestContainerSample(unittest.TestCase):
+    # Try to open a MySQL test container and run a query in it
+    # Perform validation on the query
+    pass
+
+def setUp(self):
+    pass
+
+def tearDown(self):
+    pass
+
+def test_select_query(self):
+    pass
