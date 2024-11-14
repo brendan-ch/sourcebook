d9430333c88c033ac87236f4a838df0298000982
Brendan Chen <me@bchen.dev>
2024-11-13 20:16:31 -0800

    use double quotes for everything

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello_world():  # put application"s code here
+    return "Hello World!"
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
