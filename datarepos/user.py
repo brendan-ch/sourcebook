9659371d9e74ee07a589027cc6f9e86ce7ce82ad
Brendan Chen <me@bchen.dev>
2024-11-17 21:46:50 -0800

    add stub methods for user repo

--- datarepos/user.py
+++ datarepos/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import mysql.connector
 
 from config import DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE_SCHEMA_NAME
@@ -17,3 +19,20 @@ class UserRepo:
             self.connection = connection
 
         self.connection_is_open = True
+
+    def get_user_role_in_class(self, user_id: str, class_id: str):
+        # TODO implement after class data model is created
+        raise NotImplemented
+
+    def get_user_id_if_credentials_match(self, email: str, given_password: str) -> Optional[str]:
+        # Hypothetical login flow:
+        # Receive login credentials at endpoint
+        # Pass to this method
+        # Hash the user's given password
+        # Check if email and password combination match
+        # Return the user ID to store in the user's session cookie
+        pass
+
+    def close_connection(self):
+        self.connection.close()
+        self.connection_is_open = False
