7bb9e75148e6a6cc3b75dde1a4fe1478cdb3c63d
Brendan Chen <me@bchen.dev>
2024-11-17 21:32:33 -0800

    add DATABASE_PORT to env

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_HOST = os.environ.get("DATABASE_HOST")
+DATABASE_PORT = os.environ.get("DATABASE_PORT")
 DATABASE_USER = os.environ.get("DATABASE_USER")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 DATABASE_SCHEMA_NAME = os.environ.get("DATABASE_SCHEMA_NAME")
