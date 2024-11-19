aead4b7f31563ad2548d2ced0c194071127ce964
Brendan Chen <me@bchen.dev>
2024-11-19 14:25:04 -0800

    restructure app.py with blueprint setup

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def hello_world():  # put application"s code here
-    return "Hello World!"
+    # TODO register blueprints
 
+    return app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run()
