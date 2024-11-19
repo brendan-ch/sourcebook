62f6a5bc2fbc85d4745d763ba9468e069962b091
Brendan Chen <me@bchen.dev>
2024-11-18 17:02:02 -0800

    push up helper method to add test user

--- test/datarepos/test_user_repo.py
+++ test/datarepos/test_user_repo.py
@@ -1,7 +1,7 @@
 from datarepos.user_repo import UserRepo
 from custom_exceptions import AlreadyExistsException
 from test.test_with_database_container import TestWithDatabaseContainer
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 
 from models.user import User
 
@@ -9,23 +9,6 @@ class TestUserRepo(TestWithDatabaseContainer):
     def setUp(self):
         super().setUp()
         self.user_repo = UserRepo(self.connection)
-
-    def add_sample_user_to_test_db(self):
-        new_user = User(user_id=1,
-                        full_name="Test Name",
-                        email="example@example.com")
-        sample_password = "C4x6Fc4YbxUsWtz.Luj*ECo*xv@xGkQXv_h.-khVXqvAkmgiZgCoBn*Kj_.C-e9@"
-        hashed_password = generate_password_hash(sample_password)
-        add_query = '''
-        INSERT INTO user (user_id, full_name, email, hashed_password)
-        VALUES (%s, %s, %s, %s)
-        '''
-        params = (new_user.user_id, new_user.full_name, new_user.email, hashed_password)
-        cursor = self.connection.cursor()
-        cursor.execute(add_query, params)
-        self.connection.commit()
-
-        return new_user, sample_password
 
     def test_get_user_id_if_credentials_match(self):
         new_user, sample_password = self.add_sample_user_to_test_db()
