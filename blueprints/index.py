a2ec98ee7cc7ef5316d9d8010e85605a4ee4105b
Brendan Chen <me@bchen.dev>
2024-11-19 14:34:38 -0800

    stub logic for sign in page

--- blueprints/index.py
+++ blueprints/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 index_bp = Blueprint("index", __name__)
 
@@ -7,7 +7,16 @@ def all_classes_page():
     # return render_template("all_classes.html")
     return "All classes page"
 
-@index_bp.route("/sign-in")
+@index_bp.route("/sign-in", methods=["GET", "POST"])
 def sign_in_page():
-    # return render_template("sign_in.html")
-    return "Sign in page"
+    if request.method == "GET":
+        return "Sign in page"
+        # return render_template("sign_in.html")
+    else:
+        # TODO process form data and do something
+
+        # If valid, save cookie and redirect to all classes
+        # If invalid, return sign in page but with error message
+
+        pass
+
