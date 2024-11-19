ba868fe0a3dc07c2149c4ddacdede6eb76ea0d87
Brendan Chen <me@bchen.dev>
2024-11-19 14:43:40 -0800

    register app secret key with flask

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from blueprints.index import index_bp
+from config import FLASK_APP_SECRET_KEY
 
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = FLASK_APP_SECRET_KEY
     app.register_blueprint(index_bp)
 
     return app
