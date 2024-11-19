4b874af1b5b0019ecdc6eaccf94f337833ff64b1
Brendan Chen <me@bchen.dev>
2024-11-18 16:31:14 -0800

    add course repo stub methods

--- datarepos/course_repo.py
+++ datarepos/course_repo.py
@@ -1,4 +1,17 @@
+from typing import Optional
+
 from datarepos.repo import Repo
+from models.course import Course
+
 
 class CourseRepo(Repo):
-    pass
+    def get_all_course_enrollments_for_user_id(self, user_id: int) -> list[Course]:
+        # TODO figure out how to represent enrollment and role
+        pass
+
+    def get_course_by_starting_url_if_exists(self, url: str) -> Optional[Course]:
+        pass
+
+    def get_course_by_id_if_exists(self, course_id: int) -> Optional[Course]:
+        pass
+
