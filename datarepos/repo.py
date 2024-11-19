bdc51cb49b1ad737fd11d86ea80e7027d784fffa
Brendan Chen <me@bchen.dev>
2024-11-18 16:15:37 -0800

    move Repo methods to superclass

---
+++ datarepos/repo.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+from config import DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE_SCHEMA_NAME
+
+MYSQL_DUPLICATE_ENTRY_EXCEPTION_CODE = 1062
+
+
+class Repo:
+    def __init__(self, connection = None):
+        if not connection:
+            self.connection = mysql.connector.connect(
+                host=DATABASE_HOST,
+                port=DATABASE_PORT,
+                user=DATABASE_USER,
+                password=DATABASE_PASSWORD,
+                database=DATABASE_SCHEMA_NAME
+            )
+        else:
+            self.connection = connection
+
+        self.connection_is_open = True
+
+    def close_connection(self):
+        self.connection.close()
+        self.connection_is_open = False
