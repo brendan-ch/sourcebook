258c682c1c7d41a4f066454ee2ac411a37d7781f
Brendan Chen <me@bchen.dev>
2024-11-19 14:30:19 -0800

    add placeholder for sign-in page

--- blueprints/index.py
+++ blueprints/index.py
@@ -4,5 +4,10 @@ index_bp = Blueprint("index", __name__)
 
 @index_bp.route("/")
 def all_classes_page():
-    # return render_template("all-classes.html")
+    # return render_template("all_classes.html")
     return "All classes page"
+
+@index_bp.route("/sign-in")
+def sign_in_page():
+    # return render_template("sign_in.html")
+    return "Sign in page"
