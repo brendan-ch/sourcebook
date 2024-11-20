5db634a8cb745ef2ae403aaf2885a09f391d4f70
Brendan Chen <me@bchen.dev>
2024-11-20 15:57:31 -0800

    add method to get course repository

--- flask_repository_getters.py
+++ flask_repository_getters.py
@@ -3,6 +3,7 @@ from typing import Optional
 import mysql.connector
 from flask import g, current_app
 
+from datarepos.course_repo import CourseRepo
 from datarepos.user_repo import UserRepo
 
 
@@ -17,3 +18,13 @@ def get_user_repository():
         repository = g._user_repository = UserRepo(connection)
     return repository
 
+def get_course_repository():
+    repository: Optional[CourseRepo] = getattr(g, '_course_repository', None)
+    if not repository or not repository.connection_is_open:
+        config = vars(current_app.config["DB_CONFIG_OBJECT"])
+        if not config:
+            raise RuntimeError("No database configured.")
+
+        connection = mysql.connector.connect(**config)
+        repository = g._course_repository = CourseRepo(connection)
+    return repository
