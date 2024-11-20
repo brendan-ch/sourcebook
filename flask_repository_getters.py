75a1b26d4b5eec52522de2218e202986de7a5756
Brendan Chen <me@bchen.dev>
2024-11-19 23:43:11 -0800

    use connection to db config object in current app

--- flask_repository_getters.py
+++ flask_repository_getters.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
-from flask import g
+import mysql.connector
+from flask import g, current_app
 
 from datarepos.user_repo import UserRepo
 
@@ -8,6 +9,11 @@ from datarepos.user_repo import UserRepo
 def get_user_repository():
     repository: Optional[UserRepo] = getattr(g, '_user_repository', None)
     if not repository or not repository.connection_is_open:
-        repository = g._user_repository = UserRepo()
+        config = vars(current_app.config["DB_CONFIG_OBJECT"])
+        if not config:
+            raise RuntimeError("No database configured.")
+
+        connection = mysql.connector.connect(**config)
+        repository = g._user_repository = UserRepo(connection)
     return repository
 
