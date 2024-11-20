862a8530b57387e8ba88c25d118f46efc2b8f928
Brendan Chen <me@bchen.dev>
2024-11-20 00:12:55 -0800

    implement basic version of sign-in endpoint

--- blueprints/index.py
+++ blueprints/index.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, redirect
+
+from flask_repository_getters import get_user_repository
 
 index_bp = Blueprint("index", __name__)
 
@@ -13,10 +15,18 @@ def sign_in_page():
         return "Sign in page"
         # return render_template("sign_in.html")
     else:
-        # TODO process form data and do something
+        email = request.form.get("email")
+        password = request.form.get("password")
 
-        # If valid, save cookie and redirect to all classes
-        # If invalid, return sign in page but with error message
+        if not email or not password:
+            # TODO flash message in rendering template
+            return "Please fill out all missing fields", 400
 
-        pass
+        user_repository = get_user_repository()
+        user_id = user_repository.get_user_id_if_credentials_match(email, password)
 
+        if not user_id:
+            return "Incorrect email or password, please try again", 401
+
+        session["user_id"] = user_id
+        return redirect("/")
