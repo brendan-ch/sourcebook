7934af3407c9eed7ef2bebca9bfe3ab5faf10474
Brendan Chen <me@bchen.dev>
2024-11-19 23:56:24 -0800

    update blueprint test to use test client, and add sign-in test

--- test/blueprints/test_index_blueprint.py
+++ test/blueprints/test_index_blueprint.py
@@ -1,13 +1,14 @@
+from flask_repository_getters import get_user_repository
+from models.user import User
 from test.test_flask_app import TestFlaskApp
-
 
 class TestIndexBlueprint(TestFlaskApp):
     def test_all_classes(self):
-        response = self.app.get("/")
+        response = self.test_client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_sign_in_page(self):
-        response = self.app.get("/sign-in")
+        response = self.test_client.get("/sign-in")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Sign in", response.data)
         self.assertIn(b"<index", response.data)
@@ -16,11 +17,21 @@ class TestIndexBlueprint(TestFlaskApp):
         # TODO install and use BeautifulSoup for UI testing
 
     def test_sign_in_with_correct_credentials(self):
-        # TODO set up database
+        # Set up a user in the database
+        new_user = User(
+            email="example@example.com",
+            full_name="Test Name"
+        )
 
-        response = self.app.post("/sign-in", data={
-            "email": "<EMAIL>",
-            "password": "<PASSWORD>",
+        sample_password = "f211205aea2d273d48c9a8d26c2346f9d2ec878b50f3e262b2bd86bb3f9e6cf2"
+
+        with self.app.app_context():
+            user_repository = get_user_repository()
+            new_user.user_id = user_repository.add_new_user_and_get_id(new_user, sample_password)
+
+        response = self.test_client.post("/sign-in", data={
+            "email": new_user.email,
+            "password": sample_password,
         })
 
         session_cookie = response.headers.get("Set-Cookie")
@@ -28,7 +39,7 @@ class TestIndexBlueprint(TestFlaskApp):
         self.assertIn("session=", session_cookie)
 
     def test_sign_in_with_incorrect_credentials(self):
-        response = self.app.post("/sign-in", data={
+        response = self.test_client.post("/sign-in", data={
             "email": "invalid-email@example.com",
             "password": "12345",
         })
