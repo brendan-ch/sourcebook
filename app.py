3d972c889eee09c4f134fba1e6304eae287fdb13
Brendan Chen <me@bchen.dev>
2024-11-19 23:33:07 -0800

    add custom db config param to create_app

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+from typing import Optional
+
 from flask import Flask
 
 from blueprints.index import index_bp
 from config import FLASK_APP_SECRET_KEY
+from db_connection_details import DBConnectionDetails
 
 
-def create_app():
+def create_app(custom_db_config: Optional[DBConnectionDetails] = None):
     app = Flask(__name__)
     app.secret_key = FLASK_APP_SECRET_KEY
     app.register_blueprint(index_bp)
+
+    app.config["DB_CONFIG_OBJECT"] = custom_db_config
 
     return app
 
