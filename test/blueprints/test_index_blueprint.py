628e33e240913e7792ebeb2f3ccf0ce67d3d60ee
Brendan Chen <me@bchen.dev>
2024-11-19 23:59:00 -0800

    reuse add_sample_user_to_test_db for sign in

--- test/blueprints/test_index_blueprint.py
+++ test/blueprints/test_index_blueprint.py
@@ -17,17 +17,7 @@ class TestIndexBlueprint(TestFlaskApp):
         # TODO install and use BeautifulSoup for UI testing
 
     def test_sign_in_with_correct_credentials(self):
-        # Set up a user in the database
-        new_user = User(
-            email="example@example.com",
-            full_name="Test Name"
-        )
-
-        sample_password = "f211205aea2d273d48c9a8d26c2346f9d2ec878b50f3e262b2bd86bb3f9e6cf2"
-
-        with self.app.app_context():
-            user_repository = get_user_repository()
-            new_user.user_id = user_repository.add_new_user_and_get_id(new_user, sample_password)
+        new_user, sample_password = self.add_sample_user_to_test_db()
 
         response = self.test_client.post("/sign-in", data={
             "email": new_user.email,
