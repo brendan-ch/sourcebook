e3c1d647c147b08009c8a6bca0a07b2e1b827760
Brendan Chen <me@bchen.dev>
2024-11-21 11:11:39 -0800

    add check for 401 if unauthorized

--- test/blueprints/test_course_blueprint.py
+++ test/blueprints/test_course_blueprint.py
@@ -1,6 +1,7 @@
 from flask import Response
 
 from models.course import Course
+from models.course_enrollment import CourseEnrollment, Role
 from test.test_flask_app import TestFlaskApp
 
 
@@ -20,11 +21,27 @@ class TestCourseBlueprint(TestFlaskApp):
     def test_course_home_page_content_if_enrolled(self):
         user, _ = self.add_sample_user_to_test_db()
         courses, course_terms = self.add_sample_course_term_and_course_cluster()
+        course = courses[0]
 
-        # TODO enroll the user
+        enrollment = CourseEnrollment(
+            role=Role.STUDENT,
+            user_id=user.user_id,
+            course_id=course.course_id,
+        )
+        self.add_single_enrollment(enrollment)
 
-        response = self.test_client.get(courses[0].starting_url_path)
-        self.assert_course_layout_content(response, courses[0])
+        response = self.test_client.get(course.starting_url_path + "/")
+        self.assertEqual(response.status_code, 200)
+        self.assert_course_layout_content(response, course)
 
         # TODO assert home page content
 
+    def test_course_home_page_content_if_not_enrolled_and_private(self):
+        user, _ = self.add_sample_user_to_test_db()
+        courses, course_terms = self.add_sample_course_term_and_course_cluster()
+        course = courses[0]
+
+        response = self.test_client.get(course.starting_url_path + "/")
+        self.assertEqual(response.status_code, 401)
+
+        # TODO assert 401 page content
