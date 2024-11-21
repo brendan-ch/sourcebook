db4e23838b6a5623d18789b4df6c7702c33c0d1e
Brendan Chen <me@bchen.dev>
2024-11-21 11:15:24 -0800

    add sample static page for course layout

--- blueprints/course.py
+++ blueprints/course.py
@@ -1,11 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 course_bp = Blueprint("course", __name__)
 
 @course_bp.route("/<string:course_url>/", methods=["GET"])
 def course_home_page(course_url: str):
-    # TODO render home page
-    return f"Course home page for {course_url}"
+    return render_template("course_static_page.html")
 
 @course_bp.route("/<string:course_url>/<path:custom_static_path>/", methods=["GET"])
 def course_custom_static_url_page(course_url: str, custom_static_path: str):
