1c5e434be90c52b26b9489defdb1bf1c717bbddc
Brendan Chen <me@bchen.dev>
2024-11-21 20:07:00 -0800

    add sample page content to class

--- test/datarepos/test_content_repo.py
+++ test/datarepos/test_content_repo.py
@@ -7,18 +7,8 @@ from test.test_with_database_container import TestWithDatabaseContainer
 
 
 class TestContentRepo(TestWithDatabaseContainer):
-    def setUp(self):
-        super().setUp()
-        self.content_repo = ContentRepo(self.connection)
 
-    def test_add_new_page_and_get_id(self):
-        # For a course to exist it must be linked to a course,
-        # and it *may* be linked to a user
-        user, _ = self.add_sample_user_to_test_db()
-        courses, _ = self.add_sample_course_term_and_course_cluster()
-        course = courses[0]
-
-        sample_page_content = '''
+    sample_page_content = '''
 # Home
 
 Discover the art and science of creating immersive games! This course
@@ -41,11 +31,23 @@ Embark on your journey into the exciting world of game development today!
 - [Assignments](/assignments)
 - [Contact Me](/contact-me)
 
-        '''
+'''
+
+    def setUp(self):
+        super().setUp()
+        self.content_repo = ContentRepo(self.connection)
+
+    def test_add_new_page_and_get_id(self):
+        # For a course to exist it must be linked to a course,
+        # and it *may* be linked to a user
+        user, _ = self.add_sample_user_to_test_db()
+        courses, _ = self.add_sample_course_term_and_course_cluster()
+        course = courses[0]
+
         new_page = Page(
             created_by_user_id=user.user_id,
             page_title="Home",
-            page_content=sample_page_content,
+            page_content=self.sample_page_content,
             page_visibility_setting=VisibilitySetting.LISTED,
             url_path_after_course_path="/",
             course_id=course.course_id
