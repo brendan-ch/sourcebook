17b5764e2437972b17faf1ba0a515f2778b84d34
Brendan Chen <me@bchen.dev>
2024-11-22 11:51:00 -0800

    add method to insert page into db

--- datarepos/content_repo.py
+++ datarepos/content_repo.py
@@ -1,12 +1,51 @@
 from typing import Optional
 
+from mysql.connector import IntegrityError
+
+from custom_exceptions import AlreadyExistsException
 from datarepos.repo import Repo
 from models.page import Page
 
 
 class ContentRepo(Repo):
     def add_new_page_and_get_id(self, page: Page) -> int:
-        pass
+        if page.page_id:
+            raise AlreadyExistsException
+
+        insert_page_query = '''
+        INSERT INTO page (
+            page_visibility_setting,
+            page_content,
+            page_title,
+            url_path_after_course_path,
+            course_id,
+            created_by_user_id
+        ) 
+        VALUES (%s, %s, %s, %s, %s, %s);
+        '''
+        params = (
+            page.page_visibility_setting.value,
+            page.page_content,
+            page.page_title,
+            page.url_path_after_course_path,
+            page.course_id,
+            page.created_by_user_id
+        )
+
+        cursor = self.connection.cursor()
+
+        try:
+            cursor.execute(insert_page_query, params)
+            self.connection.commit()
+        except IntegrityError as e:
+            if e.errno == self.MYSQL_DUPLICATE_ENTRY_EXCEPTION_CODE:
+                raise AlreadyExistsException
+            else:
+                raise e
+
+        id = cursor.lastrowid
+        return id
+
 
     def update_page_by_id(self, page: Page):
         pass
