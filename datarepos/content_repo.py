5a8d467868fc68a65a6db226c25088661288c5f1
Brendan Chen <me@bchen.dev>
2024-11-21 16:00:20 -0800

    add content datarepo methods

---
+++ datarepos/content_repo.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from datarepos.repo import Repo
+from models.page import Page
+
+
+class ContentRepo(Repo):
+    def add_new_page_and_get_id(self, page: Page) -> int:
+        pass
+
+    def update_page_by_id(self, page: Page):
+        pass
+
+    def delete_page_by_id(self, page_id: int):
+        pass
+
+    def delete_pages_with_course_id(self, course_id: int):
+        pass
+
+    def get_page_by_id_if_exists(self, page_id: int) -> Optional[Page]:
+        pass
+
+    def get_page_by_url_and_course_id_if_exists(self, url_path: str) -> Optional[Page]:
+        pass
+
+    def get_listed_pages_for_course_id(self, course_id: int) -> list[Page]:
+        pass
