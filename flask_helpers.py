e6fcd466f0de04fcae42bf3c05e3eaef3bc1bec3
Brendan Chen <me@bchen.dev>
2024-11-28 13:07:51 -0800

    use uuid instead of user id

--- flask_helpers.py
+++ flask_helpers.py
@@ -5,7 +5,7 @@ from flask_repository_getters import get_user_repository
 
 def get_user_from_session():
     user = None
-    if "user_id" in session and session["user_id"]:
+    if "user_uuid" in session and session["user_uuid"]:
         user_repo = get_user_repository()
-        user = user_repo.get_user_from_id_if_exists(session["user_id"])
+        user = user_repo.get_user_from_uuid_if_exists(session["user_uuid"])
     return user
