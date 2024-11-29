176b9baabc817a3e4d8023176148576cc34534bb
Brendan Chen <me@bchen.dev>
2024-11-29 14:51:15 -0800

    apply home page rendering behavior to custom pages

--- blueprints/course.py
+++ blueprints/course.py
@@ -10,6 +10,7 @@ from flask_repository_getters import get_course_repository, get_user_repository,
 from models.course import Course
 from models.course_enrollment import Role
 from models.page import VisibilitySetting, Page
+from models.user import User
 
 course_bp = Blueprint("course", __name__)
 
@@ -27,6 +28,35 @@ def generate_html_from_markdown(page: Page, course: Course):
 
     page_html_content = soup.prettify()
     return page_html_content
+
+def render_static_page_template_based_on_role(course: Course, user: User, page: Page, role: Role):
+    if not page:
+        return render_template(
+            "course_static_page.html",
+            course=course,
+            user=user,
+            role=role,
+            page_html_content="<p>This course does not have a home page.</p>",
+        ), 404
+    elif page.page_visibility_setting == VisibilitySetting.HIDDEN \
+            and role == Role.STUDENT:
+        return render_template(
+            "course_static_page.html",
+            course=course,
+            user=user,
+            role=role,
+            page_html_content="<p>This page is hidden.</p>"
+        ), 401
+
+    page_html_content = generate_html_from_markdown(page, course)
+
+    return render_template(
+        "course_static_page.html",
+        course=course,
+        user=user,
+        role=role,
+        page_html_content=page_html_content,
+    )
 
 @course_bp.route("/<string:course_url>/", methods=["GET"])
 def course_home_page(course_url: str):
@@ -53,32 +83,11 @@ def course_home_page(course_url: str):
         url_path="/"
     )
 
-    if not page:
-        return render_template(
-            "course_static_page.html",
-            course=course,
-            user=user,
-            role=role,
-            page_html_content="<p>This course does not have a home page.</p>",
-        ), 404
-    elif page.page_visibility_setting == VisibilitySetting.HIDDEN \
-        and role == Role.STUDENT:
-        return render_template(
-            "course_static_page.html",
-            course=course,
-            user=user,
-            role=role,
-            page_html_content="<p>This page is hidden.</p>"
-        ), 401
-
-    page_html_content = generate_html_from_markdown(page, course)
-
-    return render_template(
-        "course_static_page.html",
+    return render_static_page_template_based_on_role(
         course=course,
         user=user,
         role=role,
-        page_html_content=page_html_content,
+        page=page,
     )
 
 @course_bp.route("/<string:course_url>/<path:custom_static_path>/", methods=["GET"])
@@ -106,8 +115,12 @@ def course_custom_static_url_page(course_url: str, custom_static_path: str):
         url_path="/" + custom_static_path
     )
 
-    if not page:
-        return render_template("404.html"), 404
+    return render_static_page_template_based_on_role(
+        role=role,
+        course=course,
+        page=page,
+        user=user,
+    )
 
 @course_bp.route("/<string:course_url>/<path:custom_static_path>/edit/", methods=["GET", "POST"])
 def course_custom_static_url_edit_page(course_url: str, custom_static_path: str):
