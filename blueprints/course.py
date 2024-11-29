92c6e3b8977d81ae23a20cc2dd16c4c8aaa3cf1e
Brendan Chen <me@bchen.dev>
2024-11-29 14:33:36 -0800

    change rendering behavior based on role and visibility setting

--- blueprints/course.py
+++ blueprints/course.py
@@ -7,9 +7,26 @@ from flask import Blueprint, render_template, session, abort
 
 from flask_helpers import get_user_from_session
 from flask_repository_getters import get_course_repository, get_user_repository, get_content_repository
-from models.page import VisibilitySetting
+from models.course import Course
+from models.course_enrollment import Role
+from models.page import VisibilitySetting, Page
 
 course_bp = Blueprint("course", __name__)
+
+def generate_html_from_markdown(page: Page, course: Course):
+    page_html_content = markdown2.markdown(page.page_content)
+
+    soup = BeautifulSoup(page_html_content, "html.parser")
+    all_relative_links = soup.find_all("a", href=re.compile("^/"))
+    for relative_link in all_relative_links:
+        replacement = soup.new_tag("a", **relative_link.attrs)
+        replacement.string = relative_link.string
+        url_without_beginning_slash = relative_link.attrs["href"][1:]
+        replacement.attrs["href"] = urllib.parse.urljoin(course.starting_url_path + "/", url_without_beginning_slash)
+        relative_link.replace_with(replacement)
+
+    page_html_content = soup.prettify()
+    return page_html_content
 
 @course_bp.route("/<string:course_url>/", methods=["GET"])
 def course_home_page(course_url: str):
@@ -35,21 +52,26 @@ def course_home_page(course_url: str):
         course_id=course.course_id,
         url_path="/"
     )
-    if not page or page.page_visibility_setting == VisibilitySetting.HIDDEN:
-        page_html_content = "<p>This course does not have a home page.</p>"
-    else:
-        page_html_content = markdown2.markdown(page.page_content)
 
-    soup = BeautifulSoup(page_html_content, "html.parser")
-    all_relative_links = soup.find_all("a", href=re.compile("^/"))
-    for relative_link in all_relative_links:
-        replacement = soup.new_tag("a", **relative_link.attrs)
-        replacement.string = relative_link.string
-        url_without_beginning_slash = relative_link.attrs["href"][1:]
-        replacement.attrs["href"] = urllib.parse.urljoin(course.starting_url_path + "/", url_without_beginning_slash)
-        relative_link.replace_with(replacement)
+    if not page:
+        return render_template(
+            "course_static_page.html",
+            course=course,
+            user=user,
+            role=role,
+            page_html_content="<p>This course does not have a home page.</p>",
+        ), 404
+    elif page.page_visibility_setting == VisibilitySetting.HIDDEN \
+        and role == Role.STUDENT:
+        return render_template(
+            "course_static_page.html",
+            course=course,
+            user=user,
+            role=role,
+            page_html_content="<p>This page is hidden.</p>"
+        ), 401
 
-    page_html_content = soup.prettify()
+    page_html_content = generate_html_from_markdown(page, course)
 
     return render_template(
         "course_static_page.html",
@@ -61,8 +83,31 @@ def course_home_page(course_url: str):
 
 @course_bp.route("/<string:course_url>/<path:custom_static_path>/", methods=["GET"])
 def course_custom_static_url_page(course_url: str, custom_static_path: str):
-    # TODO render static page
-    return f"Custom static path for {course_url}: {custom_static_path}"
+    user = get_user_from_session()
+    course_repo = get_course_repository()
+
+    course = course_repo.get_course_by_starting_url_if_exists("/" + course_url)
+    if not course:
+        return render_template("404.html"), 404
+
+    role = None
+    if user:
+        role = course_repo.get_user_role_in_class_if_exists(user.user_id, course.course_id)
+
+    if not role:
+        return render_template(
+            "401.html",
+            custom_error_message="You need to be enrolled in this course to see it."
+        ), 401
+
+    content_repository = get_content_repository()
+    page = content_repository.get_page_by_url_and_course_id_if_exists(
+        course_id=course.course_id,
+        url_path="/" + custom_static_path
+    )
+
+    if not page:
+        return render_template("404.html"), 404
 
 @course_bp.route("/<string:course_url>/<path:custom_static_path>/edit/", methods=["GET", "POST"])
 def course_custom_static_url_edit_page(course_url: str, custom_static_path: str):
