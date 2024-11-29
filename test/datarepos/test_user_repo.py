dbad34620547085a891ca5f460dac4c617039d32
Brendan Chen <me@bchen.dev>
2024-11-29 15:37:16 -0800

    add test cases for deletion

--- test/datarepos/test_user_repo.py
+++ test/datarepos/test_user_repo.py
@@ -1,7 +1,10 @@
 import uuid
 
 from datarepos.user_repo import UserRepo
-from custom_exceptions import AlreadyExistsException
+from custom_exceptions import AlreadyExistsException, NotFoundException, DependencyException
+from models.course_enrollment import CourseEnrollment, Role
+from models.file import File
+from models.page import Page, VisibilitySetting
 from test.test_with_database_container import TestWithDatabaseContainer
 from werkzeug.security import check_password_hash
 
@@ -143,16 +146,95 @@ class TestUserRepo(TestWithDatabaseContainer):
         self.assertEqual(email, new_user.email)
 
     def test_delete_user_by_id(self):
-        pass
+        new_user, _ = self.add_sample_user_to_test_db()
+        self.user_repo.delete_user_by_id(new_user.user_id)
+
+        get_user_query = '''
+        SELECT user_id
+        FROM user
+        WHERE user_id = %s
+        '''
+        params = (new_user.user_id,)
+
+        cursor = self.connection.cursor()
+        cursor.execute(get_user_query, params)
+        cursor_result = cursor.fetchone()
+        self.assertIsNone(cursor_result)
+
 
     def test_delete_nonexistent_user_by_id(self):
-        pass
+        nonexistent_user_id = 1
+        with self.assertRaises(NotFoundException):
+            self.user_repo.delete_user_by_id(nonexistent_user_id)
 
     def test_delete_user_with_course_enrollment_dependencies(self):
-        pass
+        new_user, sample_password = self.add_sample_user_to_test_db()
+        courses, _ = self.add_sample_course_term_and_course_cluster()
+
+        enrollment = CourseEnrollment(
+            user_id=new_user.user_id,
+            course_id=courses[0].course_id,
+            role=Role.STUDENT
+        )
+        self.add_single_enrollment(enrollment)
+
+        with self.assertRaises(DependencyException):
+            self.user_repo.delete_user_by_id(new_user.user_id)
+
+        get_user_query = '''
+        SELECT user_id, full_name, email, hashed_password
+        FROM user
+        WHERE user_id = %s
+        '''
+        params = (new_user.user_id,)
+
+        cursor = self.connection.cursor()
+        cursor.execute(get_user_query, params)
+        cursor_result = cursor.fetchone()
+        self.assertIsNotNone(cursor_result)
+
+        user_id, full_name, email, hashed_password = cursor_result
+        self.assertEqual(user_id, new_user.user_id)
+        self.assertEqual(full_name, new_user.full_name)
+        self.assertEqual(email, new_user.email)
+        self.assertTrue(check_password_hash(hashed_password, sample_password))
 
     def test_delete_user_with_page_creation_dependencies(self):
-        pass
+        new_user, sample_password = self.add_sample_user_to_test_db()
+        courses, _ = self.add_sample_course_term_and_course_cluster()
+        course = courses[0]
+
+        page = Page(
+            created_by_user_id=new_user.user_id,
+            page_content="Sample content",
+            page_title="Sample title",
+            page_visibility_setting=VisibilitySetting.LISTED,
+            url_path_after_course_path="/",
+            course_id=course.course_id
+        )
+        self.add_single_page_and_get_id(page)
+
+        with self.assertRaises(DependencyException):
+            self.user_repo.delete_user_by_id(new_user.user_id)
+
+        get_user_query = '''
+        SELECT user_id, full_name, email, hashed_password
+        FROM user
+        WHERE user_id = %s
+        '''
+        params = (new_user.user_id,)
+
+        cursor = self.connection.cursor()
+        cursor.execute(get_user_query, params)
+        cursor_result = cursor.fetchone()
+        self.assertIsNotNone(cursor_result)
+
+        user_id, full_name, email, hashed_password = cursor_result
+        self.assertEqual(user_id, new_user.user_id)
+        self.assertEqual(full_name, new_user.full_name)
+        self.assertEqual(email, new_user.email)
+        self.assertTrue(check_password_hash(hashed_password, sample_password))
 
     def test_delete_user_with_file_creation_dependencies(self):
+        # TODO implement with file attachments feature
         pass
