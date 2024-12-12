681be8edc9275401d1aebbfb50ffac5d807215fc
Brendan Chen <me@bchen.dev>
2024-12-11 20:37:53 -0800

    add admin switch

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Optional
 
 from flask import Flask
@@ -9,11 +10,9 @@ from config import FLASK_APP_SECRET_KEY, DATABASE_HOST, DATABASE_SCHEMA_NAME, DA
 from db_connection_details import DBConnectionDetails
 
 
-def create_app(custom_db_config: Optional[DBConnectionDetails] = None):
+def create_app(is_admin_app = False, custom_db_config: Optional[DBConnectionDetails] = None):
     app = Flask(__name__)
     app.secret_key = FLASK_APP_SECRET_KEY
-    app.register_blueprint(index_bp)
-    app.register_blueprint(course_bp)
 
     if not custom_db_config:
         custom_db_config = DBConnectionDetails(
@@ -24,10 +23,17 @@ def create_app(custom_db_config: Optional[DBConnectionDetails] = None):
             port=DATABASE_PORT
         )
 
+    # TODO register a different set of blueprints for admins
+    app.register_blueprint(index_bp)
+    app.register_blueprint(course_bp)
+
     app.config["DB_CONFIG_OBJECT"] = custom_db_config
 
     return app
 
 if __name__ == "__main__":
+    if sys.argv[-1] == "--admin":
+        print("admin")
+
     app = create_app()
     app.run()
