b903f61d30c9e66fa4c0e13e5e04597e93e154f4
Brendan Chen <me@bchen.dev>
2024-12-12 11:28:58 -0800

    add test for deletion of attendance session and records

--- test/datarepos/test_attendance_repo.py
+++ test/datarepos/test_attendance_repo.py
@@ -32,7 +32,13 @@ class TestAttendanceRepo(TestWithDatabaseContainer):
         INSERT INTO attendance_session (course_id, title, opening_time, closing_time, attendance_session_id) 
         VALUES (%s, %s, %s, %s, %s)
         '''
-        params = astuple(attendance_session)
+        params = (
+            attendance_session.course_id,
+            attendance_session.title,
+            attendance_session.opening_time,
+            attendance_session.closing_time,
+            attendance_session.attendance_session_id,
+        )
 
         cursor = self.connection.cursor()
         cursor.execute(insert_session_query, params)
@@ -40,6 +46,21 @@ class TestAttendanceRepo(TestWithDatabaseContainer):
         self.connection.commit()
 
         return attendance_session_id
+
+    def add_single_attendance_record(self, attendance_record: AttendanceRecord):
+        insert_record_query = '''
+        INSERT INTO attendance_record (user_id, attendance_session_id, attendance_status) 
+        VALUES (%s, %s, %s)
+        '''
+        params = (
+            attendance_record.user_id,
+            attendance_record.attendance_session_id,
+            attendance_record.attendance_status.value
+        )
+
+        cursor = self.connection.cursor()
+        cursor.execute(insert_record_query, params)
+        self.connection.commit()
 
     def test_start_new_attendance_session_and_get_id(self):
         course, users = self.add_course_and_users_for_attendance_test()
@@ -126,10 +147,55 @@ class TestAttendanceRepo(TestWithDatabaseContainer):
             self.attendance_repo.close_in_progress_session(attendance_session_id)
 
     def test_delete_attendance_session_and_records(self):
-        pass
+        course, users = self.add_course_and_users_for_attendance_test()
+
+        course_id = course.course_id
+        attendance_session = AttendanceSession(
+            course_id=course_id,
+            opening_time=datetime.now(),
+            closing_time=datetime.now() + timedelta(hours=2),
+            title="Attendance Session",
+        )
+        attendance_session_id = self.add_single_attendance_session_and_get_id(attendance_session)
+
+        attendance_records = [
+            AttendanceRecord(
+                attendance_session_id=attendance_session_id,
+                user_id=user.user_id,
+                attendance_status=AttendanceRecordStatus.NONE,
+            )
+            for user in users
+        ]
+        for attendance_record in attendance_records:
+            self.add_single_attendance_record(attendance_record)
+
+        self.attendance_repo.delete_attendance_session_and_records(attendance_session_id)
+
+        check_session_query = '''
+        SELECT
+            ats.attendance_session_id
+        FROM attendance_session ats
+        WHERE ats.attendance_session_id = %s
+        '''
+        params = (attendance_session_id,)
+
+        cursor = self.connection.cursor()
+        cursor.execute(check_session_query, params)
+        result = cursor.fetchone()
+        self.assertIsNone(result)
+
+        check_records_query = '''
+        SELECT atr.user_id, atr.attendance_session_id, atr.attendance_status
+        FROM attendance_record atr
+        WHERE atr.attendance_session_id = %s
+        '''
+        cursor.execute(check_records_query, params)
+        results = cursor.fetchall()
+        self.assertEqual(len(results), 0)
 
     def test_edit_attendance_session_title(self):
         pass
 
     def test_update_attendance_record_status(self):
         pass
+
