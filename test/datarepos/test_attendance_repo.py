d12a110d5d5b8b07d4bb2a84f8ffca6765e6ef40
Brendan Chen <me@bchen.dev>
2024-12-12 10:44:28 -0800

    add test for starting new attendance session id

--- test/datarepos/test_attendance_repo.py
+++ test/datarepos/test_attendance_repo.py
@@ -1,4 +1,6 @@
 from datarepos.attendance_repo import AttendanceRepo
+from models.attendance_session import AttendanceSession
+from models.course_enrollment import CourseEnrollment, Role
 from test.test_with_database_container import TestWithDatabaseContainer
 
 
@@ -9,12 +11,33 @@ class TestAttendanceRepo(TestWithDatabaseContainer):
 
     def test_start_new_attendance_session_and_get_id(self):
         courses, _ = self.add_sample_course_term_and_course_cluster()
-        users = self.add_many_sample_users_to_test_db()
+        course = courses[0]
+
+        session_id = self.attendance_repo.start_new_attendance_session_and_get_id(course.course_id)
 
         # Check that a new session was created
         # Check that attendance records were created for
         # every student enrolled in the class
-        pass
+
+        check_session_query = '''
+        SELECT
+            ats.course_id,
+            ats.attendance_session_id,
+            ats.title,
+            ats.closing_time,
+            ats.opening_time
+        FROM attendance_session ats
+        WHERE ats.attendance_session_id = %s
+        '''
+        params = (session_id,)
+
+        cursor = self.connection.cursor()
+        cursor.execute(check_session_query, params)
+        result = cursor.fetchone()
+        returned_session = AttendanceSession(**result)
+
+        self.assertEqual(returned_session.course_id, course.course_id)
+        self.assertEqual(returned_session.attendance_session_id, session_id)
 
     def test_close_in_progress_session(self):
         # Test that a closing time was added to the data
