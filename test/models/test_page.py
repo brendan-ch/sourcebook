8b1da7ebeec1e5e218621d2dadc46e2371a8d8ef
Brendan Chen <me@bchen.dev>
2024-12-12 15:04:44 -0800

    write rest of tests

--- test/models/test_page.py
+++ test/models/test_page.py
@@ -4,7 +4,18 @@ from models.page import Page, VisibilitySetting
 
 
 class TestPage(unittest.TestCase):
+    # TODO write unit tests for other validation logic
+
     # Test page validation logic
+    def test_create_page(self):
+        Page(
+            page_title="Test Title",
+            page_content="Test Content",
+            page_visibility_setting=VisibilitySetting.LISTED,
+            url_path_after_course_path="/valid-path",
+            course_id=1
+        )
+
     def test_url_path_ends_with_slash(self):
         with self.assertRaises(ValueError):
             Page(
@@ -16,7 +27,15 @@ class TestPage(unittest.TestCase):
             )
 
     def test_url_path_startswith_forbidden_word(self):
-        pass
+        words_to_check = ["/attendance", "/new", "/foo/edit/bar"]
+        for word in words_to_check:
+            with self.subTest(word=word):
+                with self.assertRaises(ValueError):
+                    Page(
+                        page_title="Test Title",
+                        page_content="Test Content",
+                        page_visibility_setting=VisibilitySetting.LISTED,
+                        url_path_after_course_path=word,
+                        course_id=1
+                    )
 
-    def test_url_path_contains_forbidden_word(self):
-        pass
